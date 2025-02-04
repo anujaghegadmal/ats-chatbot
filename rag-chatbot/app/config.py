ed39f778020d46b0c9e6e5d8f6cf61c778092da6
anujaghegadmal <anujag2411@gmail.com>
2025-02-04 16:53:35 +0530

    weaviate keys added

--- rag-chatbot/app/config.py
+++ rag-chatbot/app/config.py
@@ -2,8 +2,9 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     mongo_uri: str = "mongodb://localhost:27017"
-    weaviate_url: str = "http://localhost:8080"
+    weaviate_url: str
     openai_api_key: str
+    wcd_api_key: str
     model_name: str = "gpt-3.5-turbo"
     
     class Config:
